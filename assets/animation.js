c6eb85d2734544dcc25570b5f759be872ab63333
marloweR02 <marlowe.ramirez02@gmail.com>
2026-05-08 22:29:19 +0800

    save

--- assets/animation.js
+++ assets/animation.js
@@ -1,20 +1,23 @@
-const fadeInUp = document.querySelectorAll('.fade-in-up')
+function initAnimation(){
+  const fadeInUp = document.querySelectorAll('.fade-in-up')
 
-const observer = new IntersectionObserver((entries, observer)=>{
-  entries.forEach((entry, index)=>{
-    if(entry.isIntersecting){
-      entry.target.classList.add('animate')
-      observer.unobserve(entry.target)
-      entry.target.style.animationDelay = `${index * 100}ms`
-    }
+  const observer = new IntersectionObserver((entries, observer)=>{
+    entries.forEach((entry, index)=>{
+      if(entry.isIntersecting){
+        entry.target.classList.add('animate')
+        observer.unobserve(entry.target)
+        entry.target.style.animationDelay = `${index * 100}ms`
+      }
+    })
+  },
+  {
+    threshold: 0.5
+  }
+  )
+
+  fadeInUp.forEach((el)=>{
+    observer.observe(el)
   })
-},
-{
-  threshold: 0.5
 }
-)
 
-fadeInUp.forEach((el)=>{
-  observer.observe(el)
-})
-
+window.initAnimation
